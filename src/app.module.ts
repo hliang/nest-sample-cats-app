587c7aafc3bebc50d083191769a7994b611d6616
hliang <hliang@users.noreply.github.com>
2023-11-13 18:17:17 -0600

    add pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [CoreModule, CatsModule],
+  imports: [CoreModule, CatsModule, LoggerModule.forRoot()],
 })
 export class AppModule {}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
+import { PinoLogger, InjectPinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class CatsService {
   private readonly cats: Cat[] = [];
+
+  constructor(
+    @InjectPinoLogger(CatsService.name)
+    private readonly logger: PinoLogger,
+) {}
 
   create(cat: Cat) {
     this.cats.push(cat);
   }
 
   findAll(): Cat[] {
+    this.logger.info("findAll started");
     return this.cats;
   }
 }
